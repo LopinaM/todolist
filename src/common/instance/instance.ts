4524fdbdbf5ec95d5b29dbaba23b20221256c96c
LopinaM <rin.ElliA@yandex.ru>
2025-02-23 13:17:56 +0500

    update

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const token = "fa57361e-c156-410e-bfa5-ffd3358a7501";
-const apiKey = "3170bb83-2f84-4a4e-a058-ac0bba401251";
-
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
-    Authorization: `Bearer ${token}`,
-    "API-KEY": apiKey,
+    Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
+    "API-KEY": process.env.REACT_APP_API_KEY,
   },
 });
